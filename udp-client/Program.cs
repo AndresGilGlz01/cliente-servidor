f15e625a47dfff89ee6d4382bea0a9cb5bd45560
AndresGlz01 <andresgilgonzalez@outlook.com>
2024-03-04 19:55:26 -0600

    simple udp client and server

--- udp-client/Program.cs
+++ udp-client/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var client = new UdpClient // Definir cliente o servidor
+{
+    EnableBroadcast = true
+};
+
+Console.WriteLine("Iniciando cliente UDP... \n");
+var endpoint = new IPEndPoint(IPAddress.Broadcast, 9630); // A donde enviar y solicitar datos
+
+Console.WriteLine("Escribe [byebye] para salir.");
+
+var mensaje = string.Empty;
+
+while (mensaje != "byebye")
+{
+    Console.Write("Ingresa el mensaje que quieres enviar: ");
+    mensaje = Console.ReadLine() ?? string.Empty;
+
+    var buffer = Encoding.UTF8.GetBytes(mensaje);
+
+    var bytesSended = client.Send(buffer, buffer.Length, endpoint);
+
+    if (bytesSended > 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("¡Mensaje éxitosamente enviado!");
+        Console.ResetColor();
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("¡Error al enviar el mensaje!");
+        Console.ResetColor();
+    }
+}
--- udp-server/Program.cs
+++ udp-server/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+var endpoint = new IPEndPoint(IPAddress.Any, 9630);
+
+var server = new UdpClient(endpoint)
+{
+    EnableBroadcast = true
+};
+
+while (true)
+{
+    var buffer = server.Receive(ref endpoint);
+
+    var datos = Encoding.UTF8.GetString(buffer);
+
+    Console.WriteLine($"[{DateTime.Now.ToString()}] - Mensaje recibido: {datos}");
+}
