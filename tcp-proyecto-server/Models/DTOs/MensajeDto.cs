9b4eb13f5736b1e5d9609923b449798e80f1cdc2
Daniela Ordoñez <201G0272@rcarbonifera.tecnm.mx>
2024-03-09 15:42:39 -0600

    Models

---
+++ tcp-proyecto-server/Models/DTOs/MensajeDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tcp_proyecto_server.Models.DTOs
+{
+    public class MensajeDto
+    {
+        public string Name { get; set; } = null!;
+        public string Message { get; set; } = null!;
+    }
+}
---
+++ tcp-proyecto-server/Models/DTOs/PictureDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace tcp_proyecto_server.Models.DTOs
+{
+    public class PictureDto
+    {
+        public string Autor { get; set; } = null!;
+        public string Subject { get; set; } = null!;
+        public string Image { get; set; } = null!;
+        public BitmapImage? img { get; set; } = null!;
+        public DateTime Date { get; set; }
+    }
+}
