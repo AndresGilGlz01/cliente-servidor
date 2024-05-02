3513b22990278f3d7ec2f244c4956055fce7cd8d
AndresGlz01 <andresgilgonzalez@outlook.com>
2024-05-02 17:30:41 -0600

    subir proyecto api y cliente

---
+++ project-client/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace project_client.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
