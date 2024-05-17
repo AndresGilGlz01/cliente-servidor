f4af45a069d16e09980963591c2fc8ab103ddbad
AndresGlz01 <andresgilgonzalez@outlook.com>
2024-05-16 21:40:48 -0600

    Vista agregar departamentos ready

--- project-client/Areas/Admin/Controllers/DepartamentosController.cs
+++ project-client/Areas/Admin/Controllers/DepartamentosController.cs
@@ -1,11 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using Newtonsoft.Json;
+
+using project_client.Areas.Admin.Models;
+
+using System.Net.Http;
+using System.Security.Claims;
+
 namespace project_client.Areas.Admin.Controllers;
 
+[Area("Admin")]
 public class DepartamentosController : Controller
 {
-    public IActionResult Agregar()
+    readonly HttpClient httpClient = new();
+
+    public async Task<IActionResult> Agregar()
     {
-        return View();
+        var viewModel = new AgregarDepartamentoViewModel();
+
+        httpClient.BaseAddress = new Uri("https://sga.api.labsystec.net/");
+
+        var userid = User.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value;
+        var response = await httpClient.GetAsync($"/api/Departamentos/{userid}");
+
+        if (!response.IsSuccessStatusCode) return View();
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        var departamentos = JsonConvert.DeserializeObject<IEnumerable<Departamentos>>(content);
+
+        if (departamentos == null) return View();
+
+        viewModel.Departamentos = departamentos;
+
+        return View(viewModel);
     }
 }
