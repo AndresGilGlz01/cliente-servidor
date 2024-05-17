b4eb3f02b2ec5e17ba26fb9ae4552613a8ce2828
AndresGlz01 <andresgilgonzalez@outlook.com>
2024-05-16 21:29:35 -0600

    incluir vista agregar departamento

---
+++ project-client/Areas/Admin/Controllers/DepartamentosController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace project_client.Areas.Admin.Controllers;
+
+public class DepartamentosController : Controller
+{
+    public IActionResult Agregar()
+    {
+        return View();
+    }
+}
