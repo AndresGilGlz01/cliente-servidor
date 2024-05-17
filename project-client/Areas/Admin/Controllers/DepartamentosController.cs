8019bdd84007a360363330f28c77203ab198103a
AndresGlz01 <andresgilgonzalez@outlook.com>
2024-05-16 23:09:44 -0600

    Editar departamento

--- project-client/Areas/Admin/Controllers/DepartamentosController.cs
+++ project-client/Areas/Admin/Controllers/DepartamentosController.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-using Newtonsoft.Json;
-
 using project_client.Areas.Admin.Models;
 
-using System.Net.Http;
 using System.Security.Claims;
+using System.Text.Json;
 
 namespace project_client.Areas.Admin.Controllers;
 
@@ -27,11 +25,34 @@ public class DepartamentosController : Controller
 
         var content = await response.Content.ReadAsStringAsync();
 
-        var departamentos = JsonConvert.DeserializeObject<IEnumerable<Departamentos>>(content);
+        var departamentos = JsonSerializer.Deserialize<IEnumerable<Departamentos>>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
 
         if (departamentos == null) return View();
 
         viewModel.Departamentos = departamentos;
+
+        return View(viewModel);
+    }
+
+    public async Task<IActionResult> Editar(int id)
+    {
+        var viewModel = new EditarDepartamentoViewModel();
+
+        httpClient.BaseAddress = new Uri("https://sga.api.labsystec.net/");
+
+        var response = await httpClient.GetAsync($"/api/departamento/{id}");
+
+        if (!response.IsSuccessStatusCode) return View();
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        var departamento = JsonSerializer.Deserialize<Departamentos>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+        if (departamento == null) return View();
+
+        viewModel.Id = departamento.Id;
+        viewModel.Nombre = departamento.Nombre;
+        viewModel.Username = departamento.Username;
 
         return View(viewModel);
     }
---
+++ project-client/Areas/Admin/Models/EditarDepartamentoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace project_client.Areas.Admin.Models;
+
+public class EditarDepartamentoViewModel
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = null!;
+    public string Username { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string DepartamentoSuperior { get; set; } = null!;
+}
