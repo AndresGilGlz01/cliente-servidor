ed60a39027281053aad4aed96d039f0c2fa4f29e
AngelPineda1 <201G0276@rcarbonifera.tecnm.mx>
2024-05-18 18:09:25 -0600

    update

--- project-client/Areas/Usuario/Controllers/HomeController.cs
+++ project-client/Areas/Usuario/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using project_client.Areas.Usuario.Models;
+using System.Security.Claims;
 
 namespace project_client.Areas.Usuario.Controllers
 {
@@ -7,10 +10,35 @@ namespace project_client.Areas.Usuario.Controllers
     [Authorize(Roles ="Usuario")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
 
-            return View();
+        private readonly HttpClient httpClient;
+        private readonly IWebHostEnvironment webHostEnvironment;
+        public HomeController(HttpClient httpClient,IWebHostEnvironment webHostEnvironment)
+        {
+            this.httpClient = httpClient;
+            this.webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            httpClient.BaseAddress = new Uri("https://sga.api.labsystec.net/");
+            var userid = User.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value;
+            var response = await httpClient.GetAsync($"/api/actividades/{userid}");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+
+                // Deserializar la cadena JSON en una lista de ActividadesViewModel
+                var actividades = JsonConvert.DeserializeObject<List<ActividadesViewModel>>(content);
+                if (actividades != null)
+                {
+
+                    List<ActividadesViewModel> acts = actividades.ToList();
+                    return View(acts);
+                }
+            }
+            return View(null);
+           
         }
     }
 }
---
+++ project-client/Areas/Usuario/Models/ActividadesViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace project_client.Areas.Usuario.Models
+{
+    public class ActividadesViewModel
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public int? IdDepartamento { get; set; }
+        public DateOnly? FechaRealizacion { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaActualizacion { get; set; }
+        public int Estado { get; set; }
+        public string Departamento { get; set; }
+    }
+}
