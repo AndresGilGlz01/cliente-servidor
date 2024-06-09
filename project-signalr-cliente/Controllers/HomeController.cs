3e074334a4a5914179f8a30d35951b0823168340
Carlos Daniel Maldonado Muñoz <201G0266@rcarbonifera.tecnm.mx>
2024-06-08 22:28:25 -0600

    Pruebas de el cliente

---
+++ project-signalr-cliente/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using project_signalr_cliente.Models.Entities;
+using project_signalr_cliente.Services;
+
+namespace project_signalr_cliente.Controllers
+{
+    public class HomeController : Controller
+    {
+        TicketsService _service;
+        Turno? _turno = new();
+        public HomeController(TicketsService service)
+        {
+            _service = service;
+            _service.ElfokinEvento += _service_ElfokinEvento;
+        }
+
+        private void _service_ElfokinEvento(object? sender, string e)
+        {
+            _turno.Numero = e;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> TurnoAsync()
+        {
+            await _service.SolicitarTurno();
+            if (!string.IsNullOrWhiteSpace(_turno.Numero))
+            {
+                return View(_turno);
+            }
+
+            return BadRequest("No jalan los turnos");
+        }
+    }
+}
--- project-signalr-cliente/Services/TicketsService.cs
+++ project-signalr-cliente/Services/TicketsService.cs
@@ -1,21 +1,41 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
+using project_signalr_cliente.Models.Entities;
 
 namespace project_signalr_cliente.Services;
 
 public class TicketsService
 {
     HubConnection hubConnection;
-
+    public event EventHandler<string> ElfokinEvento;
     public TicketsService()
     {
         hubConnection = new HubConnectionBuilder()
-            .WithUrl("https://api.signalr.labsystec.net/ticketshub")
+            //.WithUrl("https://api.signalr.labsystec.net/ticketshub")
+            .WithAutomaticReconnect()
+            .WithUrl("https://localhost:7129/ticketshub")
             .Build();
+        
+        
+        hubConnection.On<string>("TurnoNuevo", x =>
+        {
+            
+            ElfokinEvento?.Invoke(this,x);
+            //ListaTurnos.Add(x);
+        });
 
         hubConnection.Closed += async (error) =>
         {
             await Task.Delay(new Random().Next(0, 5) * 1000);
             await hubConnection.StartAsync();
         };
+        _= IniciarConeccion();
+    }
+    public async Task SolicitarTurno()
+    {
+        await hubConnection.InvokeAsync("SolicitarTurno");     
+    }
+    async Task IniciarConeccion()
+    {
+        await hubConnection.StartAsync();
     }
 }
